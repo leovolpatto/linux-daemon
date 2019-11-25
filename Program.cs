f2850e957a871240979cd8118c8c7a35dea34ac4
Leonardo Volpatto <leovolpatto@gmail.com>
2019-11-24 23:39:27 -0300

    adding f iles

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace workR
+{
+    public class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSystemd()
+                
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
